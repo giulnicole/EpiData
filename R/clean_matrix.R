27abce0f1f402d238f8ceb5a82fed702ba5c726d
giulnicole <gn.baldrighi@gmail.com>
2023-08-13 17:08:57 +0200

    building the first functions

---
+++ R/clean_matrix.R
@@ -0,0 +1,180 @@
+#' @title Data matrix cleaning for missing values after RRBS
+#' and computation of beta and M coefficients
+#'
+#' @description
+#' \code{\link{clean_matrix}} helps in the conversion of missing values (0->NA),
+#' variable types and removes rows and columns above pre-specified missingness threshold
+#'
+#'
+#' @param X Original Coverage dataframe with individuals in columns and CpGs in rows
+#' @param Y Original Methylated counts dataframe with individuals in columns and CpGs in rows
+#' @param Z Original Unmethylated counts dataframe with individuals in rows and CpG as columns
+#' @param max_na_cpg threshold of missing values per each CpG in the coverage counts matrix
+#' @param max_na_ind threshold of missing values per each individual
+#' @param cpg_removal_threshold minimum threshold of coverage counts (across all individuals) for a CpG to be kept
+#' @param p offset parameter to compute Beta matrix and M matrix
+#'
+#' @name clean_matrix
+#'
+#' @return
+#' #' list with 7 datasets:
+#'  \item{Coverage_matrix}{cleaned coverage dataset with NAs as missing values and rows/columns above the pre-specified missingness thresholds removed}
+#'  \item{Met_matrix }{methylated counts dataset with the filtered CpGs}
+#'  \item{Unmet_matrix}{unmethylated counts dataset with the filtered CpGs}
+#'  \item{Beta_matrix}{beta values matrix}
+#'  \item{M_matrix}{M values matrix}
+#'
+#'
+#'@examples
+#'
+#'
+#'
+#'
+#'
+#' @export
+#'
+#'
+clean_matrix <- function(X, Y, Z, max_na_cpg=0.5, max_na_ind=0.2,
+                         cpg_removal_threshold=10, p=100) {
+
+
+  sites <- rownames(X)
+
+  cat("Converting 0s in NAs for coverage counts ...", "\n")
+  X <- X %>% mutate_all(~na_if(., 0))
+
+  # Convert the object to a matrix (if needed)
+  is_matrix <- is.matrix(X)
+  if (!is_matrix) {
+    X <- as.matrix(X)
+  }
+
+    # Calculate the maximum allowed missing values for rows and columns
+    max_missing_rows <- ncol(X) * max_na_cpg
+    max_missing_cols <- nrow(X) * max_na_ind
+
+    # Discard CpGs with > max_na_cpg % missing values
+    row_indices_to_keep <- rowSums(is.na(X)) <= max_missing_rows
+
+    X2 <- X[row_indices_to_keep, ]
+    dim(X2)
+
+    eliminated <- nrow(X) - nrow(X2)
+    index1 <- which(row_indices_to_keep==TRUE)
+    sites_filtered1 <- sites[index1]
+    rownames(X2) <- sites_filtered1
+
+    if (eliminated != 0) {
+      message(paste(eliminated, " CpG(s) removed due to exceeding the pre-defined removal threshold (>",
+                    max_na_cpg * 100, "%) for missingness.", sep = "")) }
+
+
+
+    # Discard individuals with > max_na_ind % missing values
+    col_indices_to_keep <- colSums(is.na(X2)) <= max_missing_cols
+    X3 <- X2[, col_indices_to_keep]
+    eliminated2 <- ncol(X2) - ncol(X3)
+    index2 <- which(col_indices_to_keep==TRUE)
+
+
+    if (eliminated2 != 0) {
+      message(paste(eliminated2, " individual(s) removed due to exceeding the pre-defined removal threshold (>",
+                    max_na_ind * 100, "%) for missingness.", sep = "")) }
+
+    # Remove counts below 10 each
+    index_above_threshold <- which(apply(X3, 1, median, na.rm=T)  > cpg_removal_threshold)
+    X4 <- X3[index_above_threshold, ]
+    eliminated3 <- nrow(X3) - nrow(X4)
+
+    sites_filtered2 <- sites_filtered1[index_above_threshold]
+    rownames(X4) <- sites_filtered2
+
+
+    if (eliminated3 != 0) {
+      message(paste(eliminated3, " Cpg(s) removed due to not exceeding the pre-defined removal threshold (<",
+                    cpg_removal_threshold, ") for counts.", sep = "")) }
+
+
+
+    # Filter methylated counts and unmethylated count matrices
+    cat("Computing selection of CpGs in methylated counts matrix ...", "\n")
+
+    # Return meth and unmeth counts
+    Y <- Y[rownames(Y) %in% rownames(X4),]
+    Y <- Y[, colnames(Y) %in% colnames(X4)]
+    Y <- Y %>% mutate_all(~na_if(., 0))
+
+    cat("Computing selection of CpGs in unmethylated counts matrix ...", "\n")
+    Z <- Z[rownames(Z) %in% rownames(X4),]
+    Z <- Z[, colnames(Z) %in% colnames(X4)]
+    Z <- Z %>% mutate_all(~na_if(., 0))
+
+    # Calculating beta values matrix and M values on cleaned data
+    cat("Calculating beta values and M values ...", "\n")
+    #p <- 100  # offset parameter
+
+    # B values' matrix
+    B <- matrix(0, nrow(X4), ncol(X4))
+
+    for (j in 1:nrow(X4)){
+
+      for (i in 1:ncol(X4)) {
+
+        meth <- as.numeric(Y[[j,i]])
+        unmeth <- as.numeric(Z[[j,i]])
+
+        beta <- meth/(meth+unmeth+p)
+        B[j,i] <- beta
+
+
+      } # for i
+
+    } # for j
+
+
+    B <- as.data.frame(B)
+    colnames(B)<- colnames(X4)
+    rownames(B)<- rownames(X4)
+    cat("Beta matrix computed ...", "\n")
+
+    # M values' matrix
+    M <- matrix(0, nrow(X4), ncol(X4))
+
+    for (j in 1:nrow(X4)){
+
+      for (i in 1:ncol(X4)) {
+
+
+        meth <- as.numeric(Y[[j,i]])
+        unmeth <-  unmeth <- as.numeric(Z[[j,i]])
+
+        m <- log2(meth+p/unmeth+p)
+        M[j,i] <- m
+
+
+      } # for i
+
+    } # for j
+
+    M <- as.data.frame(M)
+
+    colnames(M)<- colnames(X4)
+    rownames(M)<- rownames(X4)
+    cat("M matrix computed ...", "\n")
+
+
+
+    # Results
+    res<- list(Coverage_matrix = as.matrix(X4), Met_matrix = Y, Unmet_matrix = Z,
+               Beta_matrix = B, M_matrix = M)
+
+    cat("Coverting results to matrices ...", "\n")
+    res2<- lapply(res, as.matrix.data.frame)
+
+
+
+    return(res2)
+
+}
+
+
---
+++ R/stats_cpg2.R
@@ -0,0 +1,154 @@
+#' @title Calculating statistics and missing values patterns
+#'
+#' @description
+#' \code{\link{stats_cpg2}} computes the statistics on missing values per dataset, highlighting the
+#' pattern of missing values per each CpG
+#'
+#'
+#' @param X Cleaned matrix list from coverage cleaning (clean_matrix)
+#' @param varselect is the index of the dataset to be used in the output from clean_matrix (numeric value 1-5)
+
+#' @name stats_cpg2
+#'
+#' @return
+#' #' list with 7 datasets:
+#'  \item{Rows}{}
+#'  \item{Columns}{}
+#'  \item{Table_missing}{}
+#'  \item{Total_NA}{}
+#'  \item{NA_per_variable}{}
+#'  \item{Fraction_missingness}{}
+#'  \item{K_table}{}
+#'  \item{MD_Pattern}{}
+#'  \item{MD_Pattern_count}{}
+#'  \item{Linear_correlation}{}
+#'  \item{long_correlation_matrix}{}
+#'
+#'@examples
+#'
+#'
+#'
+#'
+#'
+#' @export
+#'
+#'
+stats_cpg2 <- function(X, varselect = 2) {
+
+
+  options(error = expression(NULL))
+
+  cpg <- rownames(X[[varselect]])
+
+
+  # Transposing selected variable
+
+  X[[varselect]] <-t(X[[varselect]])
+  Y <- X[[varselect]]
+
+  # Extraction of dimensions of selected variable matrix
+  rows <- nrow(X[[varselect]])  # individuals
+  cols <- ncol(X[[varselect]])  # CpG
+
+
+  colnames(X[[varselect]]) <- cpg
+  names <- rownames(X[[varselect]])
+
+
+  # Applying as.numeric function to all X elements
+  vars_non_num <- (X[[varselect]])[!sapply(X[[varselect]], is.numeric)]
+  if (length(vars_non_num) != 0){
+    stop(paste("Warning! CpG(s) ", (paste(vars_non_num, collapse = ", ")),
+               " is/are not numeric. Convert this/these variables to numeric using clean_cpg() and repeat the get_data() function until no warnings are shown.",
+               sep = ""))
+
+  }
+  ### MODULE 1: table of statistics on observed values and NAs and statistics on msising values
+
+  # Statistics on selected variable values
+  # Extraction of statistics (complete observations and NAs)
+
+  # Number and percentage of complete individuals
+  no.complete <- X[[varselect]] %>% as_tibble(X[[varselect]])%>%
+    summarise_all(funs(sum(!is.na(.))))
+
+  no.complete <- as.numeric(no.complete)
+  perc.complete <- no.complete / rows * 100L
+
+  # Number and percentage of incomplete individuals
+  cat("Calculating missing values per individual ...", "\n")
+  no.incomplete <- X[[varselect]] %>% as_tibble(X[[varselect]])%>%
+    summarise_all(funs(sum(is.na(.))))
+
+  no.incomplete <- as.numeric(no.incomplete)
+  perc.incomplete <- no.incomplete / rows * 100L
+
+
+  ###
+
+  # Frequency table
+  table_missing <- data.frame(CpG = cpg,
+                              matrix(c(no.complete, perc.complete, no.incomplete,
+                                       perc.incomplete), ncol = 4L,
+                                      dimnames = list(NULL, c("nObs", "percObs", "nNA", "percNA"))),
+                                      stringsAsFactors = FALSE)
+
+
+  # Kable extra object
+  table_k <- kable(table_missing, row.names = F)  %>%
+    kable_styling(bootstrap_options = c("striped", "hover", "bordered"),
+                  full_width = T)
+
+
+  ###
+
+  # Missingness per variable
+  cat("Calculating missing values per variable ...", "\n")
+  missfrac_per_X <- sum(is.na(X[[varselect]]))/(nrow(X[[varselect]]) * ncol(X[[varselect]]))
+  missfrac_per_var <- colMeans(is.na(X[[varselect]]))
+  na_per_X <- sum(is.na(X[[varselect]]))
+  na_per_var <- sapply(X[[varselect]], function(x) sum(length(which(is.na(x)))))
+
+  # Missing data pattern
+  cat("Computing missing data pattern per variable ...", "\n")
+  mdpat <- mice::md.pattern(X[[varselect]], plot = FALSE)     # this funciton returns
+  # a matrix with \code{ncol(x)+1} columns, in which each row corresponds
+  # to a missing data pattern (1=observed, 0=missing).
+
+  mdpat <- mdpat[, colnames(mdpat) %in% cpg]
+
+  # Removing counts for leaving only the missing pattern coded as 1,0
+  mdpat_count <- mdpat[-c(1, ncol(mdpat)), ] # removing
+  #rownames(mdpat_count) <- names
+
+
+  ### MODULE 2: correlation between NAs and observed selected variable
+
+  # Complete cases to calculate correlation
+  comp <- sum(stats::complete.cases(Y))
+  mdpat_count <- as.matrix(mdpat_count)
+
+  # Linear Pearson correlation calculation (between the obeserved values)
+  cat("Computing the linear correlation matrix ...", "\n")
+  cormat.lin<- matrix(nrow = cols, ncol = cols)   # I put rows because from function stat_cpg cpg are the rows
+  colnames(cormat.lin) <- colnames(Y)
+  rownames(cormat.lin) <- colnames(Y)
+
+  cormat.lin <- stats::cor(Y, use = "pairwise.complete.obs", method = "pearson")
+
+  cat("Converting results ...", "\n")
+  long_cormat.lin <- cbind(expand.grid(dimnames(cormat.lin)), value = as.vector(cormat.lin))
+
+
+  # Results
+  res<- list(Rows = rows, Columns = cols, Table_missing = table_missing,
+             Total_NA = na_per_X, NA_per_variable = na_per_var,
+             Fraction_missingness =  missfrac_per_X,  K_table = table_k,
+             MD_Pattern = mdpat, MD_Pattern_count = mdpat_count,
+             Linear_correlation = cormat.lin,
+             long_correlation_matrix = long_cormat.lin)
+
+
+  return(res)
+
+}
