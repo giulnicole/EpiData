b0390f008cbda6639d593f13a580c97872114276
giulnicole <gn.baldrighi@gmail.com>
2023-12-12 17:34:02 +0100

    december notebook

--- notebook/example_chromosome22_part1.R
+++ notebook/example_chromosome22_part1.R
@@ -1,43 +1,97 @@
 
 # setworking directory in the EpiData package
 
-# NOTE 1 for Ali: I need to connect DESCRIPTION file to each function I created
 # some libraries are not read properly although functions are checked (Fix this)
 # library(devtools)
 # library(magrittr)
 # library(dplyr)
-
+# library(kableExtra)
+# library(GenomicRanges)
+# library(SummarizedExperiment)
+# library(tidyverse)
+# library(missForest)
+# library(FNN)
+# library(pcaMethods)
+# library(softImpute)
+# library(impute)
 
 #-------  Data
-setwd("C:/Users/gnbal/Desktop/GitHub/EpiData")
-load("data/chr22.RData")
 
+data("matrices")
 
 # Sources until functions are not installed
+devtools::load_all()
+
+#------  Example cleanMatCov
+
+# Cleaning low counts for coverage
+clean.coverage2 <- cleanCovMat(X2, Y2, Z2, max_na_cpg = 0.5, max_na_ind = 0.2)
 
 
-#------  Example clean_matrix
-source("R/clean_matrix_cov.R")
-
-# Cleaning the coverage matrix
-X2 <- X %>% replace(is.na(.), 0)
-Y2 <- Y %>% replace(is.na(.), 0)
-Z2 <- Z %>% replace(is.na(.), 0)
-clean.coverage2 <- clean_matrix_cov(X2, Y2, Z2, max_na_cpg = 0.5, max_na_ind = 0.2)
-
-
-#------  Example clean_low_coverage fopr methylated counts
-source("R/clean_counts_outliers.R")
+#------  Example cleanOutliers
 
 # Cleaning for low variance of the methylated counts (excessive NAs that occurr after clean low coverage)
-clean.new <- clean_counts_outliers(list.cleaned=clean.coverage2$Cleaned1, outlier_threshold=5, remove_outliers = TRUE)
-
+clean.new <- cleanOutliers(list.cleaned=clean.coverage2$Cleaned1, outlier_threshold=5, remove_outliers = TRUE)
 
 # match added to the function
 # clean low variance cleans the excessive NAs on methylated, and then match the same CpGs in coverage, unmethylated counts, beta matrix and M matrix
 #
 
-# NOTE 2 the examples are then to be implemented in the function body, after fixing how data are read
+#------  Example statsCpG
+stats<- stats_cpg2(clean.new$Cleaned2, varselect = 5)
 
-# NOTE 3 After fixed NOTE 1 and 2, I will add the imputation + clustering according to similarity script
-# NOTE 4 after NOTE3 is completed I will parallelize the steps for each chromosome
+
+#------  Example patternCpG
+# library(latentcor)
+# library(propagate)
+
+mat.m <- clean.new$Cleaned2$M_matrix
+means <- rowMeans(mat.m, na.rm=TRUE)
+sds <- apply(mat.m, 1, sd, na.rm=T)
+
+simu3 <- patternCpG(rownum= stats$Rows, colnum= stats$Columns,
+                     cormat= stats$Linear_correlation, meanval = means, sdval = sds,
+                     dataset = mat.m, matrix = "M")
+
+
+
+# NA replication on different methods
+mat2<- na.omit(mat.m)
+list.new<- lapply(clean.new$Cleaned2, na.omit)
+
+imp.m <- NAreplicating(list.cleaned=list.new, varselect=5, missing_prop= 0.3,
+                       scale= TRUE, n.iter= 10, sel_method= c(1:7,10,12),
+                       trees= 50, nb= 10, ncomp= 2)
+
+
+
+# Accuracy meausre
+measure_imp_met <- accuracy_measure2(imp.m)
+measure_imp_met$Boxplot.rmse
+measure_imp_met$Boxplot.mae
+
+
+
+# Pheatmap of the inizitial distance between M values of CpGs
+library(apcluster)
+m.cleaned2 <- list.new$M_matrix
+m.d0 <- negDistMat(m.cleaned2)
+pheatmap::pheatmap(as.matrix(m.d0), cluster_rows = FALSE, cluster_cols = FALSE, na_col = "black",
+                   display_numbers=F, show_colnames = F, show_rownames = F)
+
+
+stats<- stats_cpg2(list.new, varselect = 5)
+
+#------  Example patternCpG
+mat.m <- list.new$M_matrix
+means <- rowMeans(mat.m, na.rm=TRUE)
+sds <- apply(mat.m, 1, sd, na.rm=T)
+
+simu3 <- patternCpG(rownum= stats$Rows, colnum= stats$Columns,
+                    cormat= stats$Linear_correlation, meanval = means, sdval = sds,
+                    dataset = mat.m, matrix = "M")
+
+m.cleaned3 <- list.new$M_matrix
+m.d3 <- negDistMat(m.cleaned3)
+pheatmap::pheatmap(as.matrix(m.d3), cluster_rows = FALSE, cluster_cols = FALSE, na_col = "black",
+                   display_numbers=F, show_colnames = F, show_rownames = F)
