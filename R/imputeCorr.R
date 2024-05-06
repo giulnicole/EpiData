10da474ce0eb90dea9961fc1ea0c06398047c196
giulnicole <gn.baldrighi@gmail.com>
2024-05-06 10:28:42 +0200

    Update May 2

--- R/imputeCorr.R
+++ R/imputeCorr.R
@@ -21,10 +21,12 @@
 #'
 #' @examples
 #' \dontrun{
+#' # Data
 #'  data("rangedObject")
+#'  input.list<- assays(rangedObject)
 #'
 #'  # Cleaning low counts for coverage
-#'  input.list <- assays(data)
+#'
 #'  clean.coverage <- cleanCovMat(input.obj = input.list,
 #'                               max_na_cpg = 0.5, max_na_ind = 0.2,
 #'                              cpg_removal_threshold = 10)
