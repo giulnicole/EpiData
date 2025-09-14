e8b614ccf63c2a9f739391037df6ed109e6d9440
giulnicole <gn.baldrighi@gmail.com>
2025-09-14 12:02:01 +0300

    fix libraries in cleanCovMat

--- R/cleanCovMat.R
+++ R/cleanCovMat.R
@@ -3,10 +3,7 @@
 #' \code{}First part of CpGs' cleaning. `cleanCovMat` helps in the conversion of zeroes into missing values (0->NA) in the coverage matrix,
 #'  removing rows (CpGs) and columns (individuals) above pre-specified missingness threshold.
 #'
-#' @import tidyverse
-#' @import SummarizedExperiment
-#' @import stats
-#' @import htmltools
+#' @importFrom dplyr na_if
 #' @importFrom dplyr mutate_all
 #'
 #'
@@ -47,7 +44,7 @@ cleanCovMat <- function(input.obj, max_na_cpg=0.5, max_na_ind=0.2,
   sites <- rownames(X)
   stopifnot("Input must be numeric dataframe" =is.data.frame(X), all(sapply(X, is.numeric)))
   cat("Converting 0s in NAs for coverage counts ...", "\n")
-  X <- X %>% dplyr::mutate_all(~na_if(., 0))
+  X <- X %>% dplyr::mutate_all(~dplyr::na_if(., 0))
 
   # Convert the object to a matrix (if needed)
   is_matrix <- is.matrix(X)
