4e27b5349f7a4cd64503e727820657efedb13f1b
giulnicole <gn.baldrighi@gmail.com>
2025-09-21 22:26:41 +0300

    Update assays function

--- R/cleanCovMat.R
+++ R/cleanCovMat.R
@@ -21,11 +21,10 @@
 #'
 #' @examples
 #' library(dplyr)
-#' library(tidyverse)
 #' library(SummarizedExperiment)
 #' data("meth_data")
 #' print(meth_data)
-#' input.list<- assays(meth_data)
+#' input.list<- SummarizedExperiment::assays(meth_data)
 #' subset_list <- lapply(input.list, function(df) df[1:100, 1:10])
 #' clean.coverage <- cleanCovMat(input.obj = subset_list, max_na_cpg = 0.5,
 #' max_na_ind = 0.2,  cpg_removal_threshold = 10)
